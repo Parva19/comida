8f8505a12cd0e90eac30a0c5fcc181762696fad4
Mohit Chauhan <mohitchauhan626@gmail.com>
2017-10-10 16:44:17 +0530

    login change

--- app/src/main/java/gbpec/comida/LoginActivity.java
+++ app/src/main/java/gbpec/comida/LoginActivity.java
@@ -6,8 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import gbpec.comida.donor_module.Donor_NavigationMainActivity;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * A login screen that offers login via email/password.
@@ -22,25 +31,66 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
+        username=(EditText)findViewById(R.id.t_username);
+        password=(EditText)findViewById(R.id.t_password);
         signUp= (Button)findViewById(R.id.signup_bttn);
         login= (Button)findViewById(R.id.btn_login);
         signUp.setOnClickListener(this);
         login.setOnClickListener(this);
 
     }
+    private void checkLogin(String username, String password) {
+
+
+
+
+
+        String url = "http://vipul.hol.es/login_fetch.php?user_no="+username+"&password="+password;
+
+        StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                showJSON(response);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(LoginActivity.this,error.getMessage().toString(),Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+    private void showJSON(String response){
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray result = jsonObject.getJSONArray("success");
+            if(result.length()!=0)
+            {
+                Toast.makeText(LoginActivity.this,"password and id matched",Toast.LENGTH_LONG).show();
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     @Override
     public void onClick(View v) {
-        if(v.getId()== R.id.signup_bttn){
-            Intent i= new Intent(LoginActivity.this,Registration_options.class);
-           startActivity(i);
+        if (v.getId() == R.id.signup_bttn) {
+            Intent i = new Intent(LoginActivity.this, Registration_options.class);
+            startActivity(i);
         }
-        else if(v.getId()== R.id.btn_login){
+        else if (v.getId() == R.id.btn_login) {
+            String user_name = username.toString();
+            String pass_word = password.toString();
+            checkLogin(user_name, pass_word);
 
-         Intent log = new Intent(LoginActivity.this, Donor_NavigationMainActivity.class);
-            startActivity(log);
 
         }
     }
